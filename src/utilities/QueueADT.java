f1f751bf87c263e129a03f89e42b13c556344520
PrithviPathania <pathaniaprithvi1@gmail.com>
2026-06-16 23:39:25 -0600

    Notes from mondays class

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,5 +1,40 @@
 package utilities;
-
+//linked list
 public interface QueueADT {
+	
+	//v will be replaced with the real value queue holds 
 
+	/**
+	 * create empty queue (default constructor)
+	 */
+	
+	
+	/**
+	 * add to the end of the queue (enqueue)
+	 * precondition: the value to be added must not be null, and the queue must not be full.
+	 * postcondition: the value is added to the end of the queue, and the size of the queue is increased by one. the size of the queue after the value is added to the end of the queue is returned. 
+	 * @param value the value to be added to the end of the queue
+	 * @return the size of the queue after the value is added to the end of the queue
+	 * @throws NullPointerException if the value to be added is null
+	 * @
+	 */
+	int enqueue() throws NullPointerException, IllegalStateException;
+	
+	/**
+	 * remove from the front of the queue (dequeue)
+	 * precondition: a queue must exist and must not be empty.
+	 * postcondition: the value at the front of the queue is removed from the queue, and the size of the queue is decreased by one. the size of the queue after the value is removed from the front of the queue is returned.
+	 * @return the size of the queue after the value is removed from the front of the queue
+	 * 
+	 */
+	
+	int dequeue();
+	
+	/**
+	 * look up the first value within the queue (the one at the very front of the queue) (Peek)
+	 * precondition: a queue must exist and must not be empty.
+	 * postcondition: returns the value at the front of the queue without removing it from the queue.
+	 * @return the value at the front of the queue
+	 */
+	V peek();
 }
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -1,5 +1,56 @@
 package utilities;
+//array list
 
+/*
+ *  appropriate pre-conditions, post-conditions,
+	parameters, return values and expected exceptions using proper Javadoc notations
+ */
 public interface StackADT {
+
+	
+	
+    /**
+     * The ability to add to the top of a stack
+     * Precondition: the value to be added must not be null, and the stack must not be full.
+     * Postcondition: the value is added to the top of the stack, and the size of the stack is increased by one. the size of the stack after the value is added to the top of the stack is returned.
+     * @param value the value to be added to the top of the stack
+     * @throws NullPointerException if the value to be added is null
+     * @throws IllegalStateException if the stack is full
+     * @return the size of the stack after the value is added to the top of the stack
+     * 
+     *
+     */
+    void push();
+
+
+    /**
+     * The ability to remove the element from the top of the stack
+     * Precondition:
+     * Postcondition:
+     * @param
+     * @throws
+     *
+     *
+     *
+     */
+
+
+    void pop();
+
+    /**
+     * The ability to look up the first value within the stack ( the one at the very top of the stack)
+     * Precondition: A stack must exist
+     * Postcondition: returns value at the top of the stack
+     * @param
+     * @returns returns value at the top of the stack
+     */
+
+    V peek();
+
+
+
+
+
+
 
 }
